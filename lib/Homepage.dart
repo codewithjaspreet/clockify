bf9e6f5722055bd54d4e18931bd7287c0f4260ef
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-05-24 18:04:34 +0530

    ui design and math stuff

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -11,11 +11,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Clock(),
-        color: Color(0xFF2D2F41),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF2D2F41),
+        body: Container(
+          padding: EdgeInsets.all(32),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Clockify',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              Text(
+                'Clockify',
+                style: TextStyle(color: Colors.white, fontSize: 64),
+              ),
+              Clock(),
+              Text(
+                'Timezone',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.language, color: Colors.white),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text(
+                    'UTC',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          color: Color(0xFF2D2F41),
+        ),
       ),
     );
   }
--- lib/view/clock.dart
+++ lib/view/clock.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -11,18 +12,34 @@ class Clock extends StatefulWidget {
 
 class _ClockState extends State<Clock> {
   @override
+  void initState() {
+    // TODO: implement initState
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      {
+        setState(() {});
+      }
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: 300,
       height: 300,
-      child: CustomPaint(
-        painter: ClockPainter(),
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
       ),
     );
   }
 }
 
 class ClockPainter extends CustomPainter {
+  var dateTime = DateTime.now();
+  // 60 sec - 360  , 1sec - 6 degree
   @override
   void paint(Canvas canvas, Size size) {
     var centerX = size.width / 2;
@@ -34,8 +51,8 @@ class ClockPainter extends CustomPainter {
 
     var outlineBrush = Paint()
       ..strokeCap = StrokeCap.round
-      ..color = Color(0xFFEAECFF)
-      ..strokeWidth = 16
+      ..color = const Color(0xFFEAECFF)
+      ..strokeWidth = 12
       ..style = PaintingStyle.stroke;
 
     var centerFillBrush = Paint()..color = Color(0xFFEAECFF);
@@ -63,23 +80,41 @@ class ClockPainter extends CustomPainter {
       ..color = Colors.orange
       ..style = PaintingStyle.stroke
       ..strokeWidth = 8;
+    var dashBrush = Paint()
+      ..color = Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 1;
     canvas.drawCircle(center, radius - 40, fillBrush);
     canvas.drawCircle(center, radius - 40, outlineBrush);
-    var hrHandX = centerX + 60 * cos(180 * pi / 180);
-    var hrHandY = centerX + 60 * sin(180 * pi / 180);
+    var hrHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hrHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
 
     canvas.drawLine(center, Offset(hrHandX, hrHandY), hrHand);
 
-    var minHandX = centerX + 80 * cos(45 * pi / 180);
-    var minHandY = centerX + 80 * sin(45 * pi / 180);
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerX + 80 * sin(dateTime.minute * 6 * pi / 180);
     canvas.drawLine(center, Offset(minHandX, minHandY), minHand);
 
-    var secHandX = centerX + 80 * cos(90 * pi / 180);
-    var secHandY = centerX + 80 * sin(90 * pi / 180);
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
 
     canvas.drawLine(center, Offset(secHandX, secHandY), secHand);
 
     canvas.drawCircle(center, 16, centerFillBrush);
+
+    var ocr = radius;
+    var incr = radius - 14;
+    for (double i = 0; i < 360; i += 12) {
+      var x1 = centerX + ocr * cos(i * pi / 180);
+      var y1 = centerX + ocr * sin(i * pi / 180);
+      var x2 = centerX + incr * cos(i * pi / 180);
+      var y2 = centerX + incr * sin(i * pi / 180);
+
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+    }
   }
 
   @override
